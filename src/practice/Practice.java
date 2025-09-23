fd4e6ec5568a2b87eab5e8d111512b78d50a1783
Redooyyy <rwarieal@gmail.com>
2025-09-23 13:04:09 +0600

    Clean unwnted comments and unused imports

--- src/practice/Practice.java
+++ src/practice/Practice.java
@@ -1,18 +1,9 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMain.java to edit this template
- */
 package practice;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.image.Image;
 import javafx.scene.input.KeyCombination;
-import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -29,14 +20,16 @@ public class Practice extends Application {
         Scene scene = new Scene(root,Color.PURPLE);
         primaryStage.setTitle("Hello World");
         
+        
         //set height and width
         primaryStage.setHeight(420.0);
         primaryStage.setWidth(420.0);
         //set stage full screen
         primaryStage.setFullScreen(true);  //here it'll execute from full screen then after exiting full screen, it'll come to 420x420.
         //custom key for exiting full screen(by default is 'Esc' key)
-        primaryStage.setFullScreenExitHint("Press q for exit");
-        primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q"));
+        primaryStage.setFullScreenExitHint("Press q for exit"); //gives the hind
+        primaryStage.setFullScreenExitKeyCombination(KeyCombination.valueOf("q")); //exit by pressing q
+        
         
         //disable window resizable
         primaryStage.setResizable(false);
